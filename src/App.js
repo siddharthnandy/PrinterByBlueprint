ca5f22ef504cf3281a598fe4fe72d600aa67c16a
siddharthnandy <53063086+siddharthnandy@users.noreply.github.com>
2021-11-30 16:10:30 -0800

    Created PostsDisplay component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import Post from './Post';
+import PostsDisplay from './PostsDisplay';
 import './App.css';
-
-// airtable configuration
-const Airtable = require('airtable');
-
-const airtableConfig = {
-  apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
-  baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
-};
-const base = new Airtable({ apiKey: airtableConfig.apiKey })
-  .base(airtableConfig.baseKey);
 
 const App = function () {
   return (
-    <Post author="Printer User 1" body="This is a Printer post" />
+    <PostsDisplay />
   );
 };
 
--- src/PostsDisplay.js
+++ src/PostsDisplay.js
@@ -1,2 +1,27 @@
 import React from 'react';
 import Post from './Post';
+
+/*
+// airtable configuration
+const Airtable = require('airtable');
+
+const airtableConfig = {
+  apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
+  baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
+};
+
+const base = new Airtable({ apiKey: airtableConfig.apiKey })
+  .base(airtableConfig.baseKey);
+*/
+
+const PostsDisplay = function () {
+  return (
+    <>
+      <Post author="Farmer Bob" body="Farmers only" />
+      <Post author="Blue" body="Print" />
+      <Post author="Printer" body="Not twitter I swear" />
+    </>
+  );
+};
+
+export default PostsDisplay;
